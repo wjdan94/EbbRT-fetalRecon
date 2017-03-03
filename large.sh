cb2fe9b818c77f0b16119f4541afa749561f9945
Stephanie Alibrandi <smaf91@bu.edu>
2017-03-02 19:02:50 -0500

    large.sh: Add number of front end cpus

--- large.sh
+++ large.sh
@@ -7,7 +7,4 @@ set -x
 export EBBRT_NODE_ALLOCATOR_DEFAULT_CPUS=$1
 export EBBRT_NODE_ALLOCATOR_DEFAULT_RAM=32
 export EBBRT_NODE_ALLOCATOR_DEFAULT_NUMANODES=1
-./build/reconstruction -o 3TReconstruction.nii -i data/14_3T_nody_001.nii data/10_3T_nody_001.nii data/21_3T_nody_001.nii data/23_3T_nody_001.nii -m data/mask_10_3T_brain_smooth.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 1.0 --debug 0 --numThreads $1 --useCPU --iterations $2
-
-
-
+./build/reconstruction -o 3TReconstruction.nii -i data/14_3T_nody_001.nii data/10_3T_nody_001.nii data/21_3T_nody_001.nii data/23_3T_nody_001.nii -m data/mask_10_3T_brain_smooth.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 1.0 --debug 1 --numThreads $1 --useCPU --iterations $2 --numNodes $3 --numFrontEndCpus $4 
