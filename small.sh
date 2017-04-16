1b105433424897d0220baa3b8db0eb7c8bf9e3e6
Stephanie Alibrandi <smaf91@bu.edu>
2017-04-16 15:09:26 -0400

    refactor(small, large): add debug and test to running scripts

--- small.sh
+++ small.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 set -x
-#kh clean
-#sudo kill -9 $(ps aux | grep '[d]nsmasq --pid-file=/opt/khpy' | awk '{print $2}')
-#sudo kill -9 $(ps aux | grep '[q]emu-system-x86_64' | awk '{print $2}')
+if [ -z "${DEBUG}" ]
+then
+  DEBUG=0
+else
+  DEBUG=1
+fi
+
+if [ -n "${TEST}" ]
+then
+  TESTARGS=" --recIterationsFirst 1 --recIterationsLast 1"
+fi
+
 export EBBRT_NODE_ALLOCATOR_DEFAULT_CPUS=$1
 export EBBRT_NODE_ALLOCATOR_DEFAULT_RAM=4
 export EBBRT_NODE_ALLOCATOR_DEFAULT_NUMANODES=1
-./build/reconstruction -o 3TStackReconstruction.nii -i data/masked_stack-1.nii data/masked_stack-2.nii data/masked_stack-3.nii  data/masked_stack-4.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 2.0 --debug 1 --numThreads $1 --useCPU --iterations $2 --numNodes $3 --numFrontEndCpus $4 
+
+./build/reconstruction -o 3TStackReconstruction.nii -i data/masked_stack-1.nii data/masked_stack-2.nii data/masked_stack-3.nii  data/masked_stack-4.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 2.0 --debug ${DEBUG} --numThreads $1 --useCPU --iterations $2 --numNodes $3 --numFrontEndCpus $4 ${TESTARGS} | tee tmp
+
+/tmp/ebbrt/contrib/clean_running_apps.sh
